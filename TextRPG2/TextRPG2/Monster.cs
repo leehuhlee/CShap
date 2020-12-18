923618c1d3f65b04df2f7a2281b870791eda24c4
leehuhlee <leehuhlee@gmail.com>
2020-12-18 23:18:01 +0900

    Create TextRPG2

---
+++ TextRPG2/TextRPG2/Monster.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TextRPG2
+{
+    class Monster
+    {
+
+    }
+}
---
+++ TextRPG2/TextRPG2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TextRPG2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+    }
+}
