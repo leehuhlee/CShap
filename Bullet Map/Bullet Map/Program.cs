5e216bbcd394cf126f405157855cef534bb6f1cf
leehuhlee <leehuhlee@gmail.com>
2020-12-22 21:53:23 +0900

    Create BUllet Map

---
+++ Bullet Map/Bullet Map/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+
+namespace Bullet_Map
+{
+    class Program
+    {
+        class Map
+        {
+            int[,] tiles = {
+                { 1, 1, 1, 1, 1},
+                { 1, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 1},
+                { 1, 0, 0, 0, 1},
+                { 1, 1, 1, 1, 1},
+            };
+
+            public void Render()
+            {
+                ConsoleColor defaultColor = Console.ForegroundColor;
+
+                for (int y = 0; y < tiles.GetLength(1); y++)
+                {
+                    for (int x = 0; x < tiles.GetLength(0); x++)
+                    {
+                        if (tiles[y, x] == 1)
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        else
+                            Console.ForegroundColor = ConsoleColor.Green;
+                        Console.Write('\u25cf');
+                    }
+                    Console.WriteLine();
+                }
+
+                Console.ForegroundColor = defaultColor;
+            }
+        }
+        static void Main(string[] args)
+        {
+            Map map = new Map();
+            map.Render();
+        }
+    }
+}
