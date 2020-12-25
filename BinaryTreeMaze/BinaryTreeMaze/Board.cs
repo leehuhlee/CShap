12324ab1904ca91766a5abce0e08cf698e20e3b6
leehuhlee <leehuhlee@gmail.com>
2020-12-25 18:28:33 +0900

    Create Binary Tree Maze

---
+++ BinaryTreeMaze/BinaryTreeMaze/Board.cs
@@ -0,0 +1,107 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BinaryTreeMaze
+{
+    class Board
+    {
+        const char CIRCLE = '\u25cf';
+        public TileType[,] _tile;
+        public int _size;
+
+
+        public enum TileType
+        {
+            Empty,
+            Wall,
+        }
+
+        public void Initialize(int size)
+        {
+            if (size % 2 == 0)
+                return;
+
+            _tile = new TileType[size, size];
+            _size = size;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    // Make wall when x or y is enum
+                    if (x % 2 == 0 || y % 2 == 0)
+                        _tile[y, x] = TileType.Wall;
+
+                    else
+                        _tile[y, x] = TileType.Empty;
+
+                }
+            }
+
+            Random rand = new Random();
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    // Pass in wall
+                    if (x % 2 == 0 || y % 2 == 0)
+                        continue;
+
+                    // Pass in [size - 2, size - 2]
+                    if (y == _size - 2 && x == _size - 2)
+                        continue;
+
+                    // Change only right direction
+                    if (y == _size - 2)
+                    {
+                        _tile[y, x + 1] = TileType.Empty;
+                        continue;
+                    }
+
+                    // Change only bottom direction
+                    if (x == _size - 2)
+                    {
+                        _tile[y + 1, x] = TileType.Empty;
+                        continue;
+                    }
+
+                    if (rand.Next(0, 2) == 0)
+                        _tile[y, x + 1] = TileType.Empty;
+                    else
+                        _tile[y + 1, x] = TileType.Empty;
+
+                }
+            }
+        }
+
+        public void Render()
+        {
+            ConsoleColor prevColor = Console.ForegroundColor;
+
+            for (int y = 0; y < _size; y++)
+            {
+                for (int x = 0; x < _size; x++)
+                {
+                    Console.ForegroundColor = GetTileColor(_tile[y, x]);
+                    Console.Write(CIRCLE);
+                }
+                Console.WriteLine();
+            }
+        }
+
+        ConsoleColor GetTileColor(TileType type)
+        {
+            switch (type)
+            {
+                case TileType.Empty:
+                    return ConsoleColor.Green;
+                case TileType.Wall:
+                    return ConsoleColor.Red;
+                default:
+                    return ConsoleColor.Green;
+            }
+        }
+    }
+}
---
+++ BinaryTreeMaze/BinaryTreeMaze/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BinaryTreeMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            board.Initialize(25);
+
+            Console.CursorVisible = false;
+
+            while (true)
+            {
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
