6d04713cdf00c8815cbe0516e402cc11fc705c9c
leehuhlee <leehuhlee@gmail.com>
2020-12-26 19:48:30 +0900

    Create Side Winder Maze

---
+++ SideWinderMaze/SideWinderMaze/Player.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SideWinderMaze
+{
+    class Pos
+    {
+        public Pos(int y, int x) { Y = y; X = x; }
+        public int Y;
+        public int X;
+    }
+
+    class Player
+    {
+        public int PosY { get; private set; }
+        public int PosX { get; private set; }
+        Random _random = new Random();
+        Board _board;
+
+        enum Dir
+        {
+            Up = 0,
+            Left = 1,
+            Down = 2,
+            Right = 3
+        }
+
+        int _dir = (int)Dir.Up;
+        List<Pos> _points = new List<Pos>();
+
+        public void Initionalize(int posY, int posX, Board board)
+        {
+            PosY = posY;
+            PosX = posX;
+
+            _board = board;
+
+            // Position changing from current direction
+            int[] frontY = new int[] { -1, 0, 1, 0 };
+            int[] frontX = new int[] { 0, -1, 0, 1 };
+            int[] rightY = new int[] { 0, -1, 0, 1 };
+            int[] rightX = new int[] { 1, 0, -1, 0 };
+
+            _points.Add(new Pos(PosY, PosX));
+
+            // Not arrived to destination
+            while (PosY != board.DestY || PosX != board.DestX)
+            {
+                // 1. Check able to go right 
+                if (_board.Tile[PosY + rightY[_dir], PosX + rightX[_dir]] == Board.TileType.Empty)
+                {
+                    // Turn right 90 degree
+                    _dir = (_dir - 1 + 4) % 4;
+                    // Go straight 
+                    PosY = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir]; 
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                // 2. Check albe to go straight
+                else if (_board.Tile[PosY + frontY[_dir], PosX + frontX[_dir]] == Board.TileType.Empty)
+                {
+                    // Go straight
+                    PosY = PosY + frontY[_dir];
+                    PosX = PosX + frontX[_dir];
+                    _points.Add(new Pos(PosY, PosX));
+                }
+                else
+                {
+                    // Turn left 90 degree
+                    _dir = (_dir + 1 + 4) % 4;
+                }
+            }
+        }
+
+        const int MOVE_TICK = 10;
+        int _sumTick = 0;
+        int _lastIndex = 0;
+
+        public void Update(int deltaTick)
+        {
+            if (_lastIndex >= _points.Count)
+                return;
+
+            _sumTick += deltaTick;
+            if(_sumTick >= MOVE_TICK)
+            {
+                _sumTick = 0;
+
+                PosY = _points[_lastIndex].Y;
+                PosX = _points[_lastIndex].X;
+                _lastIndex++;
+                
+            }
+        }
+    }
+}
---
+++ SideWinderMaze/SideWinderMaze/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace SideWinderMaze
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Board board = new Board();
+            Player player = new Player();
+
+            board.Initialize(25, player);
+            player.Initionalize(1, 1, board);
+
+            Console.CursorVisible = false;
+
+            const int WAIT_TICK = 1000 / 30;
+            int lastTick = 0;
+
+            while (true)
+            {
+                // Frame
+                int currentTick = System.Environment.TickCount;
+                if (currentTick - lastTick < WAIT_TICK)
+                    continue;
+                int deltaTick = currentTick - lastTick;
+                lastTick = currentTick;
+                // Input
+
+                // Logic
+                player.Update(deltaTick);
+
+                // Rendering
+                Console.SetCursorPosition(0, 0);
+                board.Render();
+            }
+        }
+    }
+}
