c504bffbaee40f521f83fc8eb5bf4745b298a188
leehuhlee <leehuhlee@gmail.com>
2021-01-08 03:58:10 +0900

    Create Priority Queue

---
+++ PriorityQueue/PriorityQueue/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics.CodeAnalysis;
+
+namespace PriorityQueue
+{
+    class PriorityQueue<T> where T: IComparable<T>
+    {
+        List<T> _heap = new List<T>();
+
+        // O(logN)
+        public void Push(T data)
+        {
+            // Add new data on end
+            _heap.Add(data);
+
+            int now = _heap.Count - 1;
+
+            // Start
+            while (now > 0)
+            {
+                // Try
+                int next = (now - 1) / 2;
+                if (_heap[now].CompareTo(_heap[next]) < 0)
+                    break;
+
+                // Change
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                // Change check location
+                now = next;
+            }
+        }
+
+        // O(logN)
+        public T Pop()
+        {
+            // Save return data
+            T ret = _heap[0];
+
+            // Move last data to root
+            int lastIndex = _heap.Count - 1;
+            _heap[0] = _heap[lastIndex];
+            _heap.RemoveAt(lastIndex);
+            lastIndex--;
+
+            // Reverse
+            int now = 0;
+            while (true)
+            {
+                int left = 2 * now + 1;
+                int right = 2 * now + 2;
+
+                int next = now;
+                // If left is bigger than now, move left
+                if (left <= lastIndex && _heap[next].CompareTo(_heap[left]) < 0)
+                    next = left;
+                // If right is bigger than now, move right
+                if (right <= lastIndex && _heap[next].CompareTo(_heap[right]) < 0)
+                    next = right;
+
+                // If left/right is smaller than now, stop
+                if (next == now)
+                    break;
+
+                // change
+                T temp = _heap[now];
+                _heap[now] = _heap[next];
+                _heap[next] = temp;
+
+                // change check location
+                now = next;
+            }
+
+            return ret;
+        }
+
+        public int Count()
+        {
+            return _heap.Count;
+        }
+    }
+
+    class Knight : IComparable<Knight>
+    {
+        public int Id { get; set; }
+
+        public int CompareTo([AllowNull] Knight other)
+        {
+            if (Id == other.Id)
+                return 0;
+            return Id > other.Id ? 1 : -1;
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            PriorityQueue<Knight> q = new PriorityQueue<Knight>();
+            q.Push(new Knight(){ Id = 20 });
+            q.Push(new Knight() { Id = 10 });
+            q.Push(new Knight() { Id = 30 });
+            q.Push(new Knight() { Id = 90 });
+            q.Push(new Knight() { Id = 40 });
+
+            while(q.Count() > 0)
+            {
+                Console.WriteLine(q.Pop().Id);
+            }
+
+        }
+    }
+}
---
+++ Tree/Tree/Program.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Tree
+{
+    class TreeNode<T>
+    {
+        public T Data { get; set; }
+        public List<TreeNode<T>> Children { get; set; } = new List<TreeNode<T>>();
+    }
+
+    class Program
+    {
+        static TreeNode<string> MakeTree()
+        {
+            TreeNode<string> root = new TreeNode<string>() { Data = "R1 Development" };
+            {
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "Design" };
+                    node.Children.Add(new TreeNode<string>() { Data = "Battle" });
+                    node.Children.Add(new TreeNode<string>() { Data = "Business" });
+                    node.Children.Add(new TreeNode<string>() { Data = "Story" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "Programming" };
+                    node.Children.Add(new TreeNode<string>() { Data = "Server" });
+                    node.Children.Add(new TreeNode<string>() { Data = "Client" });
+                    node.Children.Add(new TreeNode<string>() { Data = "Engine" });
+                    root.Children.Add(node);
+                }
+                {
+                    TreeNode<string> node = new TreeNode<string>() { Data = "Art" };
+                    node.Children.Add(new TreeNode<string>() { Data = "Background" });
+                    node.Children.Add(new TreeNode<string>() { Data = "Character" });
+                    root.Children.Add(node);
+                }
+            }
+            return root;
+        }
+
+        static void PrintTree(TreeNode<string> root)
+        {
+            //Access
+            Console.WriteLine(root.Data);
+
+            foreach (TreeNode<string> child in root.Children)
+                PrintTree(child);
+        }
+
+        static int GetHeight(TreeNode<string> root)
+        {
+            int height = 0;
+            
+            foreach (TreeNode<string> child in root.Children)
+            {
+                int newHeight = GetHeight(child) + 1;
+                height = Math.Max(height, newHeight);
+            }
+
+            return height;
+        }
+
+        static void Main(string[] args)
+        {
+            TreeNode<string> root = MakeTree();
+
+            PrintTree(root);
+
+            Console.WriteLine(GetHeight(root));
+        }
+    }
+}
