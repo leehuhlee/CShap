5198b0cf50a65595a41f337ad511d19d8b16d6eb
leehuhlee <leehuhlee@gmail.com>
2021-01-26 00:51:49 +0900

    Create Contoso Pets Reverse

---
+++ ContosoPetsReverse/ContosoPetsReverse/Models/CCIPContext.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata;
+
+#nullable disable
+
+namespace ContosoPetsReverse.Models
+{
+    public partial class CCIPContext : DbContext
+    {
+        public CCIPContext()
+        {
+        }
+
+        public CCIPContext(DbContextOptions<CCIPContext> options)
+            : base(options)
+        {
+        }
+
+        public virtual DbSet<Customer> Customers { get; set; }
+        public virtual DbSet<Order> Orders { get; set; }
+        public virtual DbSet<Product> Products { get; set; }
+        public virtual DbSet<ProductOrder> ProductOrders { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+                optionsBuilder.UseSqlServer("Data Source=(localdb)\\MSSQLLocalDB;initial catalog=ContosoPets;Integrated Security=True;ConnectRetryCount=0");
+            }
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.HasAnnotation("Relational:Collation", "SQL_Latin1_General_CP1_CI_AS");
+
+            OnModelCreatingPartial(modelBuilder);
+        }
+
+        partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+    }
+}
