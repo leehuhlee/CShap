2fc4c708ece8b8a50f0ba0d1f03a3b5947705637
leehuhlee <leehuhlee@gmail.com>
2021-01-31 01:09:57 +0900

    Create Web API

---
+++ HelloAspNet/HelloEmptyWebAPI/Controllers/ValuesController.cs
@@ -0,0 +1,26 @@
+﻿using HelloEmptyWebAPI.Models;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HelloEmptyWebAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ValuesController : ControllerBase
+    {
+        [HttpGet]
+        public List<HelloMessage> Get()
+        {
+            List<HelloMessage> messages = new List<HelloMessage>();
+            messages.Add(new HelloMessage() { Message = "Hello Web API 1!" });
+            messages.Add(new HelloMessage() { Message = "Hello Web API 2!" });
+            messages.Add(new HelloMessage() { Message = "Hello Web API 3!" });
+
+            return messages;
+        }
+    }
+}
