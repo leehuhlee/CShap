ea5f728371e1db211699d7ccdc1fa3898d6a8c09
leehuhlee <leehuhlee@gmail.com>
2021-03-28 04:19:37 +0900

    GameServer: Create Thread

---
+++ Server/ServerCore/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ServerCore
+{
+    class Program
+    {
+        static void MainThread(object state)
+        {
+            for(int i=0; i<5; i++)
+                Console.WriteLine("Hello Thread!");
+        }
+
+        static void Main(string[] args)
+        {
+            ThreadPool.SetMinThreads(1, 1);
+            ThreadPool.SetMaxThreads(5, 5);
+
+            for (int i = 0; i < 5; i++)
+            {
+                Task t = new Task(() => { while (true) { } }, TaskCreationOptions.LongRunning);
+                t.Start();
+            }
+
+            ThreadPool.QueueUserWorkItem(MainThread);
+
+            while (true)
+            {
+
+            }
+        }
+    }
+}
