cbcf1eb0630259c82dcc194cf77e4567e40cc45a
leehuhlee <leehuhlee@gmail.com>
2021-03-31 05:33:44 +0900

    Game Server: Dead Lock

--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -4,6 +4,48 @@ using System.Threading.Tasks;
 
 namespace ServerCore
 {
+    class SessionManager
+    {
+        static object _lock = new object();
+
+        public static void TestSession()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                UserManager.TestUser();
+            }
+        }
+    }
+
+    class UserManager
+    {
+        static object _lock = new object();
+
+        public static void Test()
+        {
+            lock (_lock)
+            {
+                SessionManager.TestSession();
+            }
+        }
+
+        public static void TestUser()
+        {
+            lock (_lock)
+            {
+
+            }
+        }
+    }
+
     class Program
     {
         static int number = 0;
@@ -11,24 +53,17 @@ namespace ServerCore
 
         static void Thread_1()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                lock (_obj)
-                {
-                    number++;
-                }
-                // Mutual Exclusive
+                SessionManager.Test();
             }
         }
 
         static void Thread_2()
         {
-            for (int i = 0; i < 100000; i++)
+            for (int i = 0; i < 100; i++)
             {
-                lock (_obj)
-                {
-                    number--;
-                }
+                UserManager.Test();
             }
         }
 
@@ -37,6 +72,9 @@ namespace ServerCore
             Task t1 = new Task(Thread_1);
             Task t2 = new Task(Thread_2);
             t1.Start();
+
+            Thread.Sleep(1000);
+
             t2.Start();
 
             Task.WaitAll(t1, t2);
