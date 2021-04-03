a326812bdfa414a72c6d6b126138647a6f1a6cc9
leehuhlee <leehuhlee@gmail.com>
2021-04-04 06:23:01 +0900

    Game Server: Socket

--- Server/DummyClient/Program.cs
+++ Server/DummyClient/Program.cs
@@ -1,13 +1,43 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 
 namespace DummyClient
 {
     class Program
     {
-        
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            // DNS(Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
+
+            Socket socket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                socket.Connect(endPoint);
+                Console.WriteLine($"Connected To {socket.RemoteEndPoint.ToString()}");
+
+                byte[] sendBuff = Encoding.UTF8.GetBytes("Hello World!");
+                int sendBytes = socket.Send(sendBuff);
+
+                byte[] recvBuff = new byte[1024];
+                int recvBytes = socket.Receive(recvBuff);
+                string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                Console.WriteLine($"[From Server] {recvData}");
+
+                socket.Shutdown(SocketShutdown.Both);
+                socket.Close();
+            }
+            catch(Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
         }
     }
 }
--- Server/ServerCore/Program.cs
+++ Server/ServerCore/Program.cs
@@ -1,4 +1,7 @@
 ﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
 
@@ -6,24 +9,44 @@ namespace ServerCore
 {
     class Program
     {
-        static ThreadLocal<string> ThreadName = new ThreadLocal<string>(() => { return $"My Name Is {Thread.CurrentThread.ManagedThreadId}"; });
-
-        static void WhoAmI()
-        {
-            bool repeat = ThreadName.IsValueCreated;
-            if(repeat)
-                Console.WriteLine(ThreadName.Value + "(repeat)");
-            else
-                Console.WriteLine(ThreadName.Value);
-        }
-
         static void Main(string[] args)
         {
-            ThreadPool.SetMinThreads(1, 1);
-            ThreadPool.SetMaxThreads(3, 3);
-            Parallel.Invoke(WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI, WhoAmI);
+            // DNS(Domain Name System)
+            string host = Dns.GetHostName();
+            IPHostEntry ipHost = Dns.GetHostEntry(host);
+            IPAddress ipAddr = ipHost.AddressList[0];
+            IPEndPoint endPoint = new IPEndPoint(ipAddr, 7777);
 
-            ThreadName.Dispose();
+            Socket listenSocket = new Socket(endPoint.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+
+            try
+            {
+                listenSocket.Bind(endPoint);
+
+                listenSocket.Listen(10);
+
+                while (true)
+                {
+                    Console.WriteLine("Listening...");
+
+                    Socket clientSocket = listenSocket.Accept();
+
+                    byte[] recvBuff = new byte[1024];
+                    int recvBytes = clientSocket.Receive(recvBuff);
+                    string recvData = Encoding.UTF8.GetString(recvBuff, 0, recvBytes);
+                    Console.WriteLine($"[From Client] {recvData}");
+
+                    byte[] sendBuff = Encoding.UTF8.GetBytes("Welcome to MMORPG Server!");
+                    clientSocket.Send(sendBuff);
+
+                    clientSocket.Shutdown(SocketShutdown.Both);
+                    clientSocket.Close();
+                }
+            }
+            catch
+            {
+                Console.WriteLine(endPoint.ToString());
+            }
         }
     }
 }
