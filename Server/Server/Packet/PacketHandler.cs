b75935c73fa65fa02a091e9402da4e3e0e008c8d
leehuhlee <leehuhlee@gmail.com>
2021-04-19 15:30:12 +0900

    Game Server: Job Queue

--- Server/Server/Packet/PacketHandler.cs
+++ Server/Server/Packet/PacketHandler.cs
@@ -14,6 +14,7 @@ class PacketHandler
         if (clientSession.Room == null)
             return;
 
-        clientSession.Room.Broadcast(clientSession, chatPacket.chat);
+        GameRoom room = clientSession.Room;
+        room.Push(() => room.Broadcast(clientSession, chatPacket.chat));
     }
 }
