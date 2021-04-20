111d4717b1adf9fa96cccd7e2b5fcb750deb1c23
leehuhlee <leehuhlee@gmail.com>
2021-04-21 06:09:06 +0900

    Game Server: Unity

---
+++ MMO_Server/Client/Assets/Scripts/Packet/PacketHandler.cs
@@ -0,0 +1,17 @@
+﻿using DummyClient;
+using ServerCore;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using UnityEngine;
+
+class PacketHandler
+{
+    public static void S_ChatHandler(PacketSession session, IPacket packet)
+    {
+        S_Chat chatPacket = packet as S_Chat;
+        ServerSession serverSession = session as ServerSession;
+
+            Debug.Log(chatPacket.chat);
+    }
+}
